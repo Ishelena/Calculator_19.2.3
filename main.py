e1287f9dca0b22b63de187fcd1083d3954524472
Ishelena <122485100+Ishelena@users.noreply.github.com>
2023-02-25 18:30:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+В качестве тестируемого приложения выступает класс Калькулятор, выполняющий четыре математические операции: умножение, деление, вычитание, сложение.
+Файл с кодом калькулятора размещаем в корневом каталоге, в папке app.
+В папке tests находится файл с четырьмя позитивными тестами на каждую операцию.
